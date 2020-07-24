ccc8dbc0d3ecc516e524fe60e017450cad29e59b
Verma <VermaC@moodys.com>
2020-07-24 21:58:20 +0530

    sixth commit

--- src/test/java/com/moodys/meerqats/QATSManager/CreateManualCase.java
+++ src/test/java/com/moodys/meerqats/QATSManager/CreateManualCase.java
@@ -12,65 +12,93 @@ import org.testng.annotations.Test;
 
 import com.moodys.meerqats.pageclass.BaseClass;
 
-public class CreateManualCase extends BaseClass{
+public class CreateManualCase extends BaseClass {
+
 	@Test
-	public void ManualCase() throws InterruptedException{
-	
-	driver.findElement(By.xpath("//*[@id='txtUserID']")).sendKeys(prop.getProperty("ManagerUserName"));
+	public void ManualCase() throws InterruptedException {
 
-	driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(prop.getProperty("ManagerPassword"));
+		driver.findElement(By.xpath("//*[@id='txtUserID']")).sendKeys(prop.getProperty("ManagerUserName"));
 
-	driver.findElement(By.xpath("//*[@id='sub']")).click();
-	
-	driver.findElement(By.xpath("//span[text()='Create QA Review']")).click();
-	
-Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(prop.getProperty("ManagerPassword"));
 
-driver.switchTo().frame(1);
-	
-	System.out.println(driver.findElement(By.xpath("//*[@id='RULE_KEY']/div[1]/div[1]/div[1]/b")).getText());
-	
-	Actions act = new Actions(driver);
-	
-	act.sendKeys(Keys.END).build().perform();
-	
-	driver.findElement(By.xpath("//button[text()='Create Manual Case']")).click();
-	
-	Thread.sleep(2000);
-	
-	driver.findElement(By.xpath("//label[text()='Lead Analyst Name']/following-sibling::div/input[@type='text']")).sendKeys("Andreas");
-	
-	Thread.sleep(3000);
-	
-	List<WebElement>  Names= driver.findElements(By.xpath("//tr[contains(@data-gargs,'Andreas')]"));
-	
-	
-	System.out.println("Total number of names with Andreas are " + Names.size());
-	
-	for(WebElement Name: Names){
+		driver.findElement(By.xpath("//*[@id='sub']")).click();
+
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//span[text()='Create QA Review']")).click();
+
+		Thread.sleep(4000);
+
+		driver.switchTo().frame(1);
+
+		Actions act = new Actions(driver);
+
+		act.sendKeys(Keys.END).build().perform();
+
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//button[text()='Create Manual Case']")).click();
+
+		Thread.sleep(4000);
 		
-		System.out.println(Name.getText());
-		
-		if(Name.getText().contains("Olaf")){
-			
-			Name.click();
+
+		driver.findElement(By.xpath("//label[text()='Lead Analyst Name']/following-sibling::div/input[@type='text']"))
+				.sendKeys(prop.getProperty("FirstName").trim());
+
+		Thread.sleep(4000);
+
+		List<WebElement> Names = driver
+				.findElements(By.xpath("//tr[contains(@data-gargs,'" + prop.getProperty("FirstName").trim() + "')]"));
+
+		System.out.println("****************************************************************************************");
+
+		System.out.println(
+				"Total number of names with first name " + prop.getProperty("FirstName").trim() + " are " + Names.size());
+
+		System.out.println("****************************************************************************************");
+
+		System.out.println("Here are the name suggestions");
+		for (WebElement Name : Names) {
+
+			System.out.print(Name.getText());
+			System.out.println();
+
 		}
+
+		for (WebElement Name : Names) {
+
+			if (Name.getText().contains(prop.getProperty("LastName").trim())) {
+
+				Name.click();
+			}
+
+		}
+		Thread.sleep(2000);
+
+		// TODO Auto-generated catch block
+		try {
+			WebElement EmailAddress = driver
+					.findElement(By.xpath("//span[text()='Lead Analyst Email']//following-sibling::div/span"));
+
+			System.out.println("****************************************************************************************");
+
+			System.out.println(
+					"Email Address of Lead Analyst " + prop.getProperty("FirstName").trim() + " is " + EmailAddress.getText());
+		} catch (Exception e) {
+			
+			System.out.println("****************************************************************************************");
+			// TODO Auto-generated catch block
+			System.out.println("Email Address of "+prop.getProperty("FirstName").trim()+" is not populated");
+		}
+
+		Thread.sleep(4000);
 	}
-	
-	Thread.sleep(2000);
-	
-	System.out.println(driver.findElement(By.xpath("//span[text()='Lead Analyst Email']//following-sibling::div/span")).getText());
-	
 
+	@AfterClass
+	public void teardown() {
 
-}
-	
-@AfterClass
-public void teardown(){
-	
+		driver.quit();
 
-}
-
-
+	}
 
 }
